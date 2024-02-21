7e455b29346ac86ec82f2f24307883fcdafe3400
Usamovara <65997618+usamovara@users.noreply.github.com>
2024-02-21 18:34:49 +0200

    goit-js-hw-03
    first commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value){
+   let newArray=[];
+   for( let element of numbers){
+      if(element>value){
+         newArray.push(element);
+      }
+   }
+   return newArray;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
